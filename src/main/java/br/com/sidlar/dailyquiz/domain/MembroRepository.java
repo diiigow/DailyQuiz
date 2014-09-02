d9e194c080b63abf75b494b672603852036b9c8b
Rodrigo VB <rodrigosis@sidlar.com.br>
2014-09-02 13:42:52 -0300

    My second commit

---
+++ src/main/java/br/com/sidlar/dailyquiz/domain/MembroRepository.java
@@ -0,0 +1,33 @@
+package br.com.sidlar.dailyquiz.domain;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ * @author Rodrigo
+ * @since 29/08/2014
+ */
+
+public class MembroRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Membro buscaMembro(String userName, String senha) {
+        try {
+
+            TypedQuery<Membro> query = em.createQuery("select m from Membro as m where m.userName = :userName and m.senha = :senha", Membro.class);
+            query.setParameter("userName", userName);
+            query.setParameter("senha", senha);
+
+            Membro membro = query.getSingleResult();
+
+            return membro;
+
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    }
--- src/main/java/br/com/sidlar/dailyquiz/presentation/LoginController.java
+++ src/main/java/br/com/sidlar/dailyquiz/presentation/LoginController.java
@@ -1,16 +1,11 @@
 package br.com.sidlar.dailyquiz.presentation;
-
 import br.com.sidlar.dailyquiz.domain.Membro;
+import br.com.sidlar.dailyquiz.domain.MembroRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -22,9 +17,6 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/Login")
 public class LoginController {
 
-    @PersistenceContext
-    private EntityManager em;
-
     @RequestMapping(method = RequestMethod.GET)
     public String login() {
         return "/Login/login";
@@ -33,11 +25,9 @@ public class LoginController {
     @RequestMapping(method = RequestMethod.POST)
     public String loga(@RequestParam("username") String userName, @RequestParam("senha") String senha, HttpServletRequest request) {
 
-        TypedQuery<Membro> query = em.createQuery("select m from Membro as m where m.userName = :userName and m.senha = :senha", Membro.class);
-        query.setParameter("userName", userName);
-        query.setParameter("senha", senha);
+        MembroRepository membroRepository = new MembroRepository();
+        Membro membro = membroRepository.buscaMembro(userName, senha);
 
-        Membro membro = query.getSingleResult();
         request.getSession().setAttribute("membro", membro);
         return "/Home/index";
     }
